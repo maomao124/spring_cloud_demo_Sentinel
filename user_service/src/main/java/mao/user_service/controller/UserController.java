00216eea42e2b5cc7323fbc7609d3f6c1c3274c6
mao <1296193245@qq.com>
2022-07-21 19:55:41 +0800

    spring_cloud_demo_Sentinel

--- user_service/src/main/java/mao/user_service/controller/UserController.java
+++ user_service/src/main/java/mao/user_service/controller/UserController.java
@@ -36,10 +36,42 @@ public class UserController
      * @return User
      */
     @GetMapping("/{id}")
-    public User queryById(@PathVariable("id") Long id, @RequestHeader(name = "key1",required = false) String key1)
+    public User queryById(@PathVariable("id") Long id, @RequestHeader(name = "key1", required = false) String key1)
     {
         //log.debug("user被访问了："+id);
         log.info("请求头key1：" + key1);
+        if (id == 1)
+        {
+            //慢调用
+            //一定概率触发
+            if (Math.random() < 0.3)
+            {
+                try
+                {
+                    //100毫秒，大于50毫秒
+                    Thread.sleep(100);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        else if (id == 2)
+        {
+            //异常
+            //一定概率触发
+            if (Math.random() < 0.5)
+            {
+                throw new RuntimeException("熔断策略-异常比例");
+            }
+        }
+        else if (id == 3)
+        {
+            //异常
+            //一定触发
+            throw new RuntimeException("熔断策略-异常数");
+        }
         return userService.queryById(id);
     }
 }
